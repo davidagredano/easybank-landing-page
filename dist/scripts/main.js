9e10acb3e46abeea345cd962fac6992f37b6212f
David <davidagredano@gmail.com>
2023-03-20 22:35:08 +0100

    ready for deployment

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -4,13 +4,7 @@ const heroImageBgSrc = {
   desktop: "./images/bg-intro-desktop.svg",
 };
 
-// on first load
-if (window.innerWidth <= 375) {
-  heroImageBg.setAttribute("src", heroImageBgSrc.mobile);
-} else {
-  heroImageBg.setAttribute("src", heroImageBgSrc.desktop);
-}
-
+// Change hero image depending on screen resize
 window.addEventListener("resize", () => {
   if (window.innerWidth <= 375) {
     heroImageBg.setAttribute("src", heroImageBgSrc.mobile);
@@ -19,66 +13,59 @@ window.addEventListener("resize", () => {
   }
 });
 
-// const nav = document.getElementById("navbar");
-// const main = document.getElementById("main-doc");
-// const hamburgerMenu = document.getElementById("hamburgerMenu");
-// const sections = Array.from(document.getElementsByClassName("main-section"));
+if (window.innerWidth <= 375) {
+  heroImageBg.setAttribute("src", heroImageBgSrc.mobile);
+} else {
+  heroImageBg.setAttribute("src", heroImageBgSrc.desktop);
+} // this is for the first load
 
-// const menuStateActiveAdd = () => {
-//   nav.classList.add("l-nav--active");
-//   main.style.filter = "blur(2px)";
-//   hamburgerMenu.innerText = "✕";
-// };
+// Hamburger menu interaction
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const hamburgerMenuIconSrc = {
+  hamburger: "./images/icon-hamburger.svg",
+  close: "./images/icon-close.svg",
+};
+const nav = document.querySelector(".nav");
+const main = document.querySelector(".l-main");
+const overlay = document.querySelector(".overlay");
 
-// const menuStateActiveRemove = () => {
-//   nav.classList.remove("l-nav--active");
-//   main.style.filter = "none";
-//   hamburgerMenu.innerText = "☰";
-// };
+const showMenu = () => {
+  nav.classList.remove("nav--hidden");
+  const currentYPosition = window.scrollY;
+  const navCurrentTop = currentYPosition + 88 + "px";
+  nav.style.top = navCurrentTop;
+  overlay.style.display = "block";
+  hamburgerMenu.setAttribute("src", hamburgerMenuIconSrc.close);
+};
 
-// const menuToggle = () => {
-//   const navIsActive = nav.classList.contains("l-nav--active");
-//   if (navIsActive) {
-//     menuStateActiveRemove();
-//   } else {
-//     menuStateActiveAdd();
-//   }
-// };
+const hideMenu = () => {
+  nav.classList.add("nav--hidden");
+  nav.style.top = "-201px";
+  overlay.style.display = "none";
+  hamburgerMenu.setAttribute("src", hamburgerMenuIconSrc.hamburger);
+};
 
-// hamburgerMenu.addEventListener("click", menuToggle);
+const toggleMenu = () => {
+  const navIsVisible = !nav.classList.contains("nav--hidden");
+  if (navIsVisible) {
+    hideMenu();
+  } else {
+    showMenu();
+  }
+};
 
-// main.addEventListener("click", menuStateActiveRemove);
+hamburgerMenu.addEventListener("click", toggleMenu);
+main.addEventListener("click", hideMenu);
 
-// const navLinks = Array.from(document.getElementsByClassName("nav__link"));
-// navLinks.forEach((navLink) => {
-//   navLink.addEventListener("click", menuStateActiveRemove);
-// });
+window.addEventListener("resize", () => {
+  const navIsVisible = !nav.classList.contains("nav--hidden");
+  if (window.innerWidth >= 768 && navIsVisible) {
+    hideMenu();
+  }
+});
 
-// window.addEventListener("resize", () => {
-//   const navIsActive = nav.classList.contains("l-nav--active");
-//   if (window.innerWidth > 768 && navIsActive) {
-//     menuStateActiveRemove();
-//   }
-// });
-
-// window.addEventListener("scroll", () => {
-//   const viewportHeight = (n) => (window.innerHeight * n) / 100;
-//   const currentYPosition = window.scrollY + viewportHeight(30);
-//   const navListItems = Array.from(
-//     document.getElementsByClassName("nav__list-item")
-//   );
-//   navListItems.forEach((navListItem) => {
-//     const sectionId = navListItem.firstElementChild.getAttribute("href");
-//     const section = document.querySelector(sectionId);
-//     const sectionYPositionStart = section.offsetTop;
-//     const sectionYPositionEnd = section.offsetTop + section.offsetHeight;
-//     if (
-//       sectionYPositionStart <= currentYPosition &&
-//       sectionYPositionEnd > currentYPosition
-//     ) {
-//       navListItem.classList.add("nav__list-item--highlight");
-//     } else {
-//       navListItem.classList.remove("nav__list-item--highlight");
-//     }
-//   });
-// });
+window.addEventListener("scroll", () => {
+  const currentYPosition = window.scrollY;
+  const navCurrentTop = currentYPosition + 88 + "px";
+  nav.style.top = navCurrentTop;
+});
